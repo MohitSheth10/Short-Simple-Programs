57548ccaeb9e4dfbd128eeec8686558e3a1a8d22
Mohit Sheth <magicpotter2009@gmail.com>
2026-09-23 22:30:11 +0530

    Enhance encryption to support case and special characters
    Updated encryption logic to handle uppercase letters and non-alphabetic characters.

--- Encryption/Encryption key.py
+++ Encryption/Encryption key.py
@@ -3,7 +3,13 @@ word=input("Enter word u want to encrypt and send:")
 key= int(input("Enter value by which u want to encrypt the text. "))  #:)
 encrypted_word=""
 for letter in (word):
-    i=abc.index(letter)
-    newi=i+key
-    encrypted_word= encrypted_word +abc[newi]
+    if letter.lower() in abc:
+        i=abc.index(letter.lower())
+        newi=(i+key)%26  # wrap around to the start after z
+        newletter=abc[newi]
+        if letter.isupper():
+            newletter=newletter.upper()
+        encrypted_word= encrypted_word +newletter
+    else:
+        encrypted_word= encrypted_word +letter  # keep spaces, numbers and symbols as they are
 print(encrypted_word)
